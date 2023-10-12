ebcfff48cab2382878e0fa5e1bddfa01e0acf5d6
Henrycke Schenberk - Parallax <Oschenberk@gmail.com>
2023-10-12 16:35:29 -0300

    ESP32-AP

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,1 +1,11 @@
-#include "base/balanca.h"
+#ifdef FIRST_OTA
+    #include "modules/ota.h"
+#endif
+
+#ifdef BALANCA
+    #include "base/balanca.h"
+#endif
+
+#ifdef AP
+    #include "base/ap.h"
+#endif
